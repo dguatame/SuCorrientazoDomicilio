e97160d5def724f9b2d056ea35db25d345ea02b6
Diego Alberto Guatame Reina <dguatame@gmail.com>
2021-01-18 11:56:56 -0500

    Add test for goForward method

--- SuCorrientazo.Test/DeliveriesTests.cs
+++ SuCorrientazo.Test/DeliveriesTests.cs
@@ -97,5 +97,157 @@ namespace SuCorrientazoDomicilio.Test
             Assert.That(ex.Message, Is.EqualTo("El listado de direcciones está vacío"));
         }
 
+
+        [Test()]
+        public void ShouldChangeCardinalPoint_SetCardinalPoint()
+        {
+            PositionModel position = new PositionModel
+            {
+                X = 0,
+                Y = 0,
+                CardinalPoint = "NORTE"
+            };
+            MainClass.SetCardinalPoint('I', position);
+            Assert.AreEqual("OESTE", position.CardinalPoint);
+
+            MainClass.SetCardinalPoint('I', position);
+            Assert.AreEqual("SUR", position.CardinalPoint);
+
+            MainClass.SetCardinalPoint('I', position);
+            Assert.AreEqual("ESTE", position.CardinalPoint);
+
+            MainClass.SetCardinalPoint('I', position);
+            Assert.AreEqual("NORTE", position.CardinalPoint);
+
+            MainClass.SetCardinalPoint('D', position);
+            Assert.AreEqual("ESTE", position.CardinalPoint);
+
+            MainClass.SetCardinalPoint('D', position);
+            Assert.AreEqual("SUR", position.CardinalPoint);
+
+            MainClass.SetCardinalPoint('D', position);
+            Assert.AreEqual("OESTE", position.CardinalPoint);
+
+            MainClass.SetCardinalPoint('D', position);
+            Assert.AreEqual("NORTE", position.CardinalPoint);
+        }
+
+        [Test()]
+        public void ShouldNotChangeCardinalPoint_SetCardinalPoint()
+        {
+            PositionModel position = new PositionModel
+            {
+                X = 0,
+                Y = 0,
+                CardinalPoint = "NORTE"
+            };
+            MainClass.SetCardinalPoint('A', position);
+            Assert.AreEqual("NORTE", position.CardinalPoint);
+        }
+
+        [Test()]
+        public void ShouldNotThrowException_SetCardinalPoint()
+        {
+            Assert.DoesNotThrow(() => MainClass.SetCardinalPoint('I', _position));
+            Assert.DoesNotThrow(() => MainClass.SetCardinalPoint('D', _position));
+        }
+
+        [Test()]
+        public void ShouldThrowExceptionEmpty_SetCardinalPoint()
+        {
+            var ex = Assert.Throws<Exception>(() => MainClass.SetCardinalPoint(' ', _position));
+            Assert.That(ex.Message, Is.EqualTo("El comando viene vacío"));
+        }
+
+        [Test()]
+        public void ShouldThrowExceptionInvalid_SetCardinalPoint()
+        {
+            char direction = 'Z';
+            var ex = Assert.Throws<Exception>(() => MainClass.SetCardinalPoint(direction, _position));
+            Assert.That(ex.Message, Is.EqualTo($"{direction} no es un comando válido"));
+        }
+
+        [Test()]
+        public void ShouldThrowException_GoForward()
+        {
+            PositionModel position = null;
+            var ex = Assert.Throws<Exception>(() => MainClass.GoForward(position));
+            Assert.That(ex.Message, Is.EqualTo("La posición está vacía"));
+        }
+
+        [Test()]
+        public void GoForwardNorthOnePosition_GoForward()
+        {
+            PositionModel position = new PositionModel
+            {
+                X = 0,
+                Y = 0,
+                CardinalPoint = "NORTE"
+            };
+            PositionModel expected = new PositionModel
+            {
+                X = 0,
+                Y = 1,
+                CardinalPoint = "NORTE"
+            };
+            PositionModel actual = MainClass.GoForward(position);
+            Assert.AreEqual(expected.Y, actual.Y);
+            Assert.AreEqual(expected.X, actual.X);
+            Assert.AreEqual(expected.CardinalPoint, actual.CardinalPoint);
+
+
+            position = new PositionModel
+            {
+                X = 0,
+                Y = 0,
+                CardinalPoint = "SUR"
+            };
+            expected = new PositionModel
+            {
+                X = 0,
+                Y = -1,
+                CardinalPoint = "SUR"
+            };
+            actual = MainClass.GoForward(position);
+            Assert.AreEqual(expected.Y, actual.Y);
+            Assert.AreEqual(expected.X, actual.X);
+            Assert.AreEqual(expected.CardinalPoint, actual.CardinalPoint);
+
+            position = new PositionModel
+            {
+                X = 0,
+                Y = 0,
+                CardinalPoint = "OESTE"
+            };
+            expected = new PositionModel
+            {
+                X = -1,
+                Y = 0,
+                CardinalPoint = "OESTE"
+            };
+            actual = MainClass.GoForward(position);
+            Assert.AreEqual(expected.Y, actual.Y);
+            Assert.AreEqual(expected.X, actual.X);
+            Assert.AreEqual(expected.CardinalPoint, actual.CardinalPoint);
+
+            position = new PositionModel
+            {
+                X = 0,
+                Y = 0,
+                CardinalPoint = "ESTE"
+            };
+            expected = new PositionModel
+            {
+                X = 1,
+                Y = 0,
+                CardinalPoint = "ESTE"
+            };
+            actual = MainClass.GoForward(position);
+            Assert.AreEqual(expected.Y, actual.Y);
+            Assert.AreEqual(expected.X, actual.X);
+            Assert.AreEqual(expected.CardinalPoint, actual.CardinalPoint);
+        }
+
+
     }
 }
