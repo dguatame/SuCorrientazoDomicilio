a2afbf53c7cd4840410a7413f963041b9861f9fd
Diego Alberto Guatame Reina <dguatame@gmail.com>
2021-01-18 11:27:01 -0500

    Initial commit

---
+++ SuCorrientazoApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SuCorrientazoApp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
